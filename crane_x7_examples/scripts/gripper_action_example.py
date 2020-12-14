48e0759c66c05508cdd846add07a882ab0a1b2b4
kazumakumagai <71998849+kazumakumagai@users.noreply.github.com>
2020-12-14 12:11:36 +0900

    Update gripper_action_example.py

--- crane_x7_examples/scripts/gripper_action_example.py
+++ crane_x7_examples/scripts/gripper_action_example.py
@@ -80,7 +80,7 @@ def main():
         target_pose.position.x = te_x_position_vertical
         target_pose.position.y = te_y_position_vertical
         target_pose.position.z = te_z_position_vertical
-        q = quaternion_from_euler(-3.14/3.0, 3.14, -3.14/2.0)  
+        q = quaternion_from_euler(-3.14/1.5, 3.14, -3.14/2.0)  
         target_pose.orientation.x = q[0]
         target_pose.orientation.y = q[1]
         target_pose.orientation.z = q[2]
